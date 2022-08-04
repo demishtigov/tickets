5557bc41a7790e40eb9c3f95f37920a0cc9e78fd
Magomed <93042393+demishtigov@users.noreply.github.com>
2022-08-04 15:35:04 +0300

    get the data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import Header from './components/header/Header';
+import Filter from "./components/filter/Filter";
+import Header from "./components/header/Header";
+import data from "./tickets.json";
+import { useState, } from "react";
 
 const App = () => {
-  return <>
-    <Header />
-  </>
-}
+  const newData = data.tickets.slice()
+  const [ticektsData, setTicketsData] = useState(newData);
+  
+  return (
+    <>
+      <div className="contaier">
+        <Header />
+        <Filter />
+      </div>
+    </>
+  );
+};
 
 export default App;
